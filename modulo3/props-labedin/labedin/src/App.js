2498798432b988b36ee64106f6adf40e7b4c0020
Paulo <paulojunior.10@outlook.com>
2022-04-24 22:46:50 -0300

    projeto labenu

--- modulo3/props-labedin/labedin/src/App.js
+++ modulo3/props-labedin/labedin/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import CardGrande from './components/CardGrande/CardGrande';
+// import CardPequeno from './components/CardPequeno/CardPequeno'; se eu colocar esse import o site não funciona
 import ImagemButton from './components/ImagemButton/ImagemButton';
 
 function App() {
@@ -9,9 +10,9 @@ function App() {
       <div className="page-section-container">
         <h2>Dados pessoais</h2>
         <CardGrande 
-          imagem="https://uploads-ssl.webflow.com/5d640f4558306be99cf47a0e/5dd57846babb597b77c6bb1d_PerfilFuture4_cor.png" 
-          nome="Astrodev" 
-          descricao="Oi, eu sou o Astrodev. Sou o chefe dos alunos da Labenu. Adoro pedir e-mails na sexta-feira e esperar os alunos responderem só para responder com uma bronca e dar mais trabalho para eles."
+          imagem="https://media-exp1.licdn.com/dms/image/D4E03AQFt_tdoR8X8hg/profile-displayphoto-shrink_200_200/0/1643719092222?e=1656547200&v=beta&t=nn1VozfGh1Hi5z7nnlAwTMHb6TvklYmSs__Dt-bpKcQ" 
+          nome="Paulo Luiz Silva dos Santos Junior" 
+          descricao="Oi, eu sou o PJ. Sou um estudante de programação com sede de mudar o mundo ao meu redor através da tecnologia."
         />
         
         <ImagemButton 
@@ -20,25 +21,27 @@ function App() {
         />
       </div>
 
+     
+
       <div className="page-section-container">
         <h2>Experiências profissionais</h2>
         <CardGrande 
-          imagem="https://s3.amazonaws.com/future4.com.br/static/headf4-c492117ca2373dc85ca81bf715b3dc2a.png" 
-          nome="Labenu" 
-          descricao="Formando desenvolvedores para o mercado de trabalho!" 
+          imagem="https://www.ecowd.inf.br/cms/img/dropzone_upload/resized_115f5f7fb08d140_1600094128_310x.jpeg" 
+          nome="Industrial Porto Rico" 
+          descricao="Trabalhando como apontador de guia na área de transporte de caminhão de cana de ácucar!" 
         />
         
         <CardGrande 
-          imagem="https://imagens.canaltech.com.br/empresas/4418.400.jpg" 
-          nome="NASA" 
-          descricao="Apontando defeitos." 
+          imagem="https://www.paranacooperativo.coop.br/ppc/images/Comunicacao/2021/noticias/10/29/ibge/ibge_29_10_2021.JPG" 
+          nome="IBGE" 
+          descricao="Agente Censitário Municipal, supervisando a equipe de trabalho dos supervisores e responsável pela parte administrativa do posto de coleta." 
         />
       </div>
 
       <div className="page-section-container">
         <h2>Minhas redes sociais</h2>
         <ImagemButton 
-          imagem="https://d2v9ipibika81v.cloudfront.net/uploads/sites/261/2017/01/facebook-logo-3.png" 
+          imagem="https://d2v9ipibika81v.cloudfront.net/uploads/sites/261/2017/01/facebook--3.png" 
           texto="Facebook" 
         />        
 
