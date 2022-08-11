04d3d748e8feecd1d043ee1b12ddba908b479d1b
PauloJunior <paulojunior.10@outlook.com>
2022-08-11 19:28:50 -0300

    exercicio1 feito

---
+++ modulo5/aprofundamento-typescript/src/index.ts
@@ -0,0 +1,45 @@
+// Exercicío 1
+
+// a)
+// let minhaString: string = 12 se tentar executar fala que o tipo número não é disponível para o tipo string
+
+// b)
+// let meuNumero: number | string = "Olá mundo"
+// console.log(meuNumero)
+
+// c) e d)
+// enum cores {
+//     VERMELHO = "vermelho",
+//     LARANJA = "laranja",
+//     AMARELO = "amarelo",
+//     VERDE = "verde",
+//     AZUL = "azul",
+//     ANIL = "anil",
+//     VIOLETA = "violeta"
+// }
+
+// type person = {
+//     nome: string,
+//     idade: number,
+//     corfavorita: cores
+// }
+
+// const pessoa1: person = {
+//     nome: "Paulo",
+//     idade:29,
+//     corfavorita: cores.AMARELO
+// }
+
+// const pessoa2: person = {
+//     nome: "Alan",
+//     idade:39,
+//     corfavorita: cores.ANIL
+// }
+
+// const pessoa3: person = {
+//     nome: "Julio",
+//     idade:34,
+//     corfavorita: cores.VIOLETA
+// }
+
+// console.table(pessoa1)
