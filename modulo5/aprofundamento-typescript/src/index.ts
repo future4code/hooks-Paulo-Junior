7f37daef5ede217a48e4fa92e83d29c94083a0b9
PauloJunior <paulojunior.10@outlook.com>
2022-08-11 21:53:30 -0300

    exercicio2feito

--- modulo5/aprofundamento-typescript/src/index.ts
+++ modulo5/aprofundamento-typescript/src/index.ts
@@ -43,3 +43,40 @@
 // }
 
 // console.table(pessoa1)
+
+// EXERCICIO2
+
+// function obterEstatisticas (numeros: number[]) {
+
+//     const numerosOrdenados = numeros.sort(
+//         (a: number, b: number) => a - b
+//     )
+
+//     let soma = 0
+
+//     for (let num of numeros) {
+//         soma += num
+//     }
+
+//     const estatisticas = {
+//         maior: numerosOrdenados[numeros.length - 1],
+//         menor: numerosOrdenados[0],
+//         media: soma / numeros.length
+//     }
+
+//     return estatisticas
+// }
+
+// type amostraDeDados = {
+//     numeros: [21, 18, 65, 44, 15, 18] ,
+//     obterEstatisticas: (numeros: number[]) => {}
+// }
+
+
+// console.log(obterEstatisticas([21, 18, 65, 44, 15, 18] ))
+// EXERCICIO 3
+
+type post = {
+    autor: "string",
+    texto: "string"
+}
